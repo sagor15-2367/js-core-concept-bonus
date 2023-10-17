a54556f316b01a42e1d5ae1e0a7c891086e578bc
Md Sagor Alam <80034569+sagor15-2367@users.noreply.github.com>
2023-10-17 23:38:54 +0600

    k

--- argument.js
+++ argument.js
@@ -1,9 +1,16 @@
 function add(num1, num2) {
     // console.log(num1,num2);1
+
+
+    
     // console.log(arguments);2
     console.log(arguments[4]);
 }
 // add(12,13);1
+
+
+
+
 
 // add(12,13,12,33,32);2
 
